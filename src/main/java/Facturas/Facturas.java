189132b494f405c06a1d19332e569f33ac8c0acd
Aps09 <Aps09@users.noreply.github.com>
2017-03-05 21:15:27 +0100

    Add files via upload

--- src/main/java/Facturas/Facturas.java
+++ src/main/java/Facturas/Facturas.java
@@ -1,6 +1,7 @@
 package Facturas;
 
 import Clientes.Clientes;
+import Fecha.Fecha;
 
 /**
  * Created by al341853 on 28/02/17.
@@ -20,33 +21,38 @@ public class Facturas {
 
     public Facturas(){
         size=0;
+    }
+
+    public void addFactura(){
+        // recorrer vector y añadir ultimo
+    }
+
+    public int calcularFactura (){
 
     }
 
-
-    public int calculeInvoice (){
-
-    }
-
-    public Facturas.FechaFac getFecha(){
+    public Fecha getFecha(){
 
     }
 
-    public Facturas getFactura (int codfac){
+    public void getFactura (int codfac){
         for(int index = 0; index < size; index++){
             if (listaFacturas[index].codfac == codfac)
                 listaFacturas[index].getInfo();
         }
     }
 
-    public Facturas getFacturas (Clientes cliente){
-
+    public void printFacturas(){
+        for(int index = 0; index < size; index++){
+            listaFacturas[index].getInfo();
+        }
     }
 
 
     // **************************** ENSEÑAR INFORMATION ****************************
 
     public void getInfo(){
+        for(int index = 0; index < )
         System.out.println("Código de factura: "+this.codfac);
         System.out.println("Tarifa contratada: "+this.tarifa);
         System.out.println("Importe total: "+this.importe);
