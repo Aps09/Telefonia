9c4e95675e3fe8cc27c99c6277d5d66aae6cb329
KamyFullMetal <al341840@uji.es>
2017-03-05 16:29:28 +0100

    Clases de los Test creadas, falta el código

--- src/test/java/ClientesTest/ClientesTest.java
+++ src/test/java/ClientesTest/ClientesTest.java
@@ -1,7 +1,12 @@
 package ClientesTest;
 
+// import es.uji.www.GeneradorDatosINE;
+import static org.junit.*;
+
 /**
  * Created by kamy on 5/03/17.
  */
 public class ClientesTest {
+
+
 }
--- src/test/java/ClientesTest/DireccionTest.java
+++ src/test/java/ClientesTest/DireccionTest.java
@@ -1,7 +1,44 @@
 package ClientesTest;
 
+import es.uji.www.GeneradorDatosINE;
+import Clientes.Direccion;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by kamy on 5/03/17.
  */
 public class DireccionTest {
+
+    public static String provincia;
+    public static String poblacion;
+    public static int codPostal;
+    public static Direccion domicilio;
+
+    @BeforeClass
+    public static void antes{
+        GeneradorDatosINE datos = new GeneradorDatosINE;
+        provincia = datos.getProvincia();
+        poblacion = datos.getPoblacion();
+        codPostal = 12004;
+        domicilio = new Direccion(provincia, poblacion,codPostal);
+    }
+
+    @Test
+    public void pruebaGetCodPostal(){
+        assertEquals(domicilio.getCodPostal(), codPostal);
+    }
+
+    @Test
+    public void pruebaGetProvincia(){
+        assertEquals(domicilio.getProvincia(), provincia);
+    }
+
+    @Test
+    public void pruebaGetPoblacion(){
+        assertEquals(domicilio.getPoblacion(), poblacion);
+    }
+
 }
